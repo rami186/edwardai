276bf204169a915958f11727962ba4383c256983
Amr <amrmourad27@gmail.com>
2018-01-27 23:33:13 -0500

    Complete add policy to user.

--- client/js/policy.js
+++ client/js/policy.js
@@ -1,19 +1,46 @@
-function selectPolicy(policyNumber) {
-    $.ajax({
-        method: 'POST',
-        url: "https://25nmupukve.execute-api.us-west-2.amazonaws.com/prod/addpolicy",
-        headers: {
-            Authorization: authToken
-        },
-        data: JSON.stringify({
-            PolicyId: policyNumber
-        }),
-        contentType: 'application/json',
-        success: completeRequest,
-        error: function ajaxError(jqXHR, textStatus, errorThrown) {
-            console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
-            console.error('Response: ', jqXHR.responseText);
-            alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
+// Register click handler for #request button
+$(function onDocReady() {
+    $('#universityForm').submit(selectPolicy);
+
+    var authToken;
+    WildRydes.authToken.then(function setAuthToken(token) {
+        if (token) {
+            authToken = token;
+        } else {
+            window.location.href = '/signin.html';
         }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '/signin.html';
     });
-}
+
+    function selectPolicy() {
+        policyNumber = $('#select-policy').val();
+
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeUrl + '/addpolicy',
+            headers: {
+                Authorization: authToken
+            },
+            data: JSON.stringify({
+                PolicyId: policyNumber
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
+            }
+        });
+    }
+
+    function completeRequest() {
+        alert('done');
+    }
+
+    if (!_config.api.invokeUrl) {
+        $('#noApiMessage').show();
+    }
+});
